ca69e982ea25076a52ee46710257681c59835e5d
xdb <136187300@qq.com>
2018-06-24 23:32:52 +0800

    2

--- test_template/urls.py
+++ test_template/urls.py
@@ -1,4 +1,4 @@
-"""test_csrf URL Configuration
+"""test_template URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
